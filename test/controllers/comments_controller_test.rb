b875700728ea6384b39a9a82265a0449d49a6d12
lukasikb <lukasikbed@onet.pl>
2021-07-29 14:24:31 +0200

    Added tests for articles controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,8 +2,20 @@ require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
     test "should create comment" do
+      article = articles(:one)
+      comment = comments(:one)
         assert_difference('Comment.count') do
-          post comments_url, params: { comment: { body: @comment.body, commenter: @comment.commenter } }
+          post  "/articles/#{article.id}/comments", params: { comment: { body: comment.body, commenter: comment.commenter } }
+        end
+        assert_redirected_to article_path(article)
+    end
+
+    test "should delete comment" do
+      article = articles(:one)
+      comment = comments(:one)
+        assert_difference('Comment.count', -1) do
+          delete "/articles/#{article.id}/comments/#{comment.id}"
         end
     end
+
 end
